60a7a449b4a4eccd35464ae4b9197a4d23523399
Baz Lever <bazlever6@gmail.com>
2020-07-11 10:50:16 +1000

    Folders

---
+++ GMTKHiBackImGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int fireRate;
+    public int shotgunForce;
+    float fireTime;
+    bool hasFired;
+
+
+
+    void Start()
+    {
+        hasFired = false;
+    }
+
+
+    void Update()
+    {
+
+        fireTime += Time.deltaTime;
+        if (fireTime >= fireRate)
+        {
+            shoot();
+            fireTime -= fireRate;
+        }
+
+
+
+    }
+
+
+
+     void shoot()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * -shotgunForce);
+    }
+}
