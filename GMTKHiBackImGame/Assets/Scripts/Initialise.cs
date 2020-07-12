74c88043be0988ebf7c26d845e3570f295e90077
Baz Lever <bazlever6@gmail.com>
2020-07-12 21:54:15 +1000

    startup

---
+++ GMTKHiBackImGame/Assets/Scripts/Initialise.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Initialise : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        gameObject.GetComponent<AudioSource>().Stop();
+        gameObject.GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene(0);
+    }
+}
