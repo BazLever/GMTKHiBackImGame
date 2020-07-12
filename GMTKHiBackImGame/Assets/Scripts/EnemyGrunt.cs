2482814682c71f221f172fef43ab5a6989c35614
Baz Lever <bazlever6@gmail.com>
2020-07-12 15:00:16 +1000

    Mystuff

---
+++ GMTKHiBackImGame/Assets/Scripts/EnemyGrunt.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGrunt : MonoBehaviour
+{
+
+    GameObject player;
+
+    public float fireRate;
+    public GameObject projectile;
+
+    public float projectileSpeed;
+
+    public Transform projectileSpawn;
+
+    float fireDelta;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+
+    void Update()
+    {
+        fireDelta += Time.deltaTime;
+
+        transform.LookAt(player.transform);
+
+
+        RaycastHit hit;
+
+        float distanceOfRay = 200;
+
+        if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, distanceOfRay))
+        {
+            if (hit.transform.name == "Player")
+            {
+                ShootAtPlayer();
+            }
+        }
+
+
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+
+    void ShootAtPlayer()
+    {
+
+        if (fireDelta >= fireRate)
+        {
+            GameObject GO = Instantiate(projectile, projectileSpawn.position, Quaternion.identity) as GameObject;
+            GO.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * projectileSpeed, ForceMode.Impulse);
+            fireDelta = 0;
+        }
+
+        
+    }
+
+}
---
+++ GMTKHiBackImGame/Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            collider.gameObject.GetComponent<PlayerController>().Death();
+        }
+    }
+}
